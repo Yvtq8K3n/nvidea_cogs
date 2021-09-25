9b5e91b02662c16cf4ae96b5174fc12f75c737d6
yvtq8k3n <yvtq8k3n@gmail.com>
2021-09-25 17:03:30 +0100

    Improving code clarifty

--- voicerecognition/voicerecognition.py
+++ voicerecognition/voicerecognition.py
@@ -8,41 +8,53 @@ class VoiceRecognition(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.active = False
+        self.mute = True
 
+    # Para cada comando 
     async def voiceMiddleware(self, ctx, error, success):
-        if not hasattr(ctx.author.voice, 'channel'):
+        isUserInVoice = hasattr(ctx.author.voice, 'channel')
+        isBotPresent = self.active
+
+        if not isUserInVoice :
             await ctx.send("You are not in a voice channel.")
-        elif len(self.bot.voice_clients) == 0:
-            await error()
-        elif ctx.author.voice.channel == ctx.voice_client.channel:
-            await success()
         else:
-            await ctx.send("We are not in the same voice channel.")
+            if not isBotPresent:
+                self.active = True
+                author = ctx.message.author
+                channel = author.voice.channel
+                await channel.connect()
+            else:
+                voiceChannelMatch = ctx.author.voice.channel == ctx.voice_client.channel
+                if (voiceChannelMatch):
+                    await success()
+                else:
+                    await error()
 
     # [p] means command prefix
     # when user uses [p]toggle
     @commands.command()
     async def toggle(self, ctx):
+
         async def error():
-            self.active = True
-            author = ctx.message.author
-            channel = author.voice.channel
-            await channel.connect()
-            await changeSelfDeafen(ctx, False)
-            await ctx.send("Voice recognition enabled.")
+            await ctx.send("We are not in the same voice channel.")
+            
         async def success():
-            await changeSelfDeafen(ctx, self.active)
-            self.active = not self.active
+            self.mute = not self.mute
+            await changeSelfDeafen(ctx, self.mute)
             await ctx.send("Voice recognition enabled." if self.active else "Voice recognition disabled.")
+
         await self.voiceMiddleware(ctx, error, success)
 
     # when user uses [p]exit
     @commands.command()
     async def exit(self, ctx):
+
         async def error():
             await ctx.send("I am not in a voice channel.")
+
         async def success():
-            await changeSelfDeafen(ctx, True)
             await ctx.voice_client.disconnect()
+            self.active = False
+
         await self.voiceMiddleware(ctx, error, success)
         
